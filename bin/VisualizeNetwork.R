8cc678dc5b2bfbae44ca452b54a8deae88f57822
Geoffrey Hannigan <ghanni@upenn.edu>
2016-02-04 12:37:09 -0500

    Updated netowrk vis to color interactive pairs by type.

--- bin/VisualizeNetwork.R
+++ bin/VisualizeNetwork.R
@@ -17,20 +17,16 @@ graph = startGraph("http://localhost:7474/db/data/")
 
 # Use Cypher query to get a table of the table edges
 query="
-START n=node(*) MATCH (n)-[r]->(m) RETURN n.Name AS from, m.Name AS to;
+START n=node(*) MATCH (n)-[r]->(m) RETURN n.Name AS from, m.Name AS to, type(r) AS type;
 "
 edges = cypher(graph, query)
 
-#test <- as.data.frame(t(as.data.frame(sapply(strsplit(edges[,2], ' '), head,2))))
-#edges$to <- paste(test$V1, test$V2, sep=" ")
-
-#colnames(edges) <- c("from","to")
-
-# Remove the edges to singleton nodes
-SingletonNodes <- ddply(edges, c("to"), summarize, length=length(to))
-# Subset because the it is not visible with all small clusters
-SingletonNodesRemoved <- SingletonNodes[c(SingletonNodes$length > 0),]
-MultipleEdge <- edges[c(which(edges$to %in% SingletonNodesRemoved$to)),]
+## Remove the edges to singleton nodes
+#SingletonNodes <- ddply(edges, c("to"), summarize, length=length(to))
+## Subset because the it is not visible with all small clusters
+#SingletonNodesRemoved <- SingletonNodes[c(SingletonNodes$length > 0),]
+#MultipleEdge <- edges[c(which(edges$to %in% SingletonNodesRemoved$to)),]
+MultipleEdge <- edges
 
 # Set nodes
 nodes = data.frame(id=unique(c(MultipleEdge$from, MultipleEdge$to)))
@@ -43,7 +39,7 @@ V(ig)$label = ifelse(grepl("[Pp]hage", nodes$id), "", nodes$id)
 V(ig)$label = ""
 V(ig)$color = ifelse(grepl("[Pp]hage", nodes$id), rgb(0,0,1,.75), rgb(1,0,0,.75))
 #V(ig)$color <- rgb(0,1,0,.2)
-E(ig)$color <- rgb(.5,.5,.5,.2)
+E(ig)$color <- ifelse(grepl("Infects", edges$type), rgb(0.5,0.5,0.5,.75), rgb(0.5,0,0,.75))
 E(ig)$width <- 0.01
 V(ig)$frame.color <- NA
 V(ig)$label.color <- rgb(0,0,.2,.5)

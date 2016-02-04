0ef1ca709c5c21d34395c08ecac1b466a4f94143
Geoffrey Hannigan <ghanni@upenn.edu>
2016-02-04 12:37:54 -0500

    Finished prepping Pfam annotation and interactions. Time to run it.

--- bin/PfamDomainInteractPrediction.sh
+++ bin/PfamDomainInteractPrediction.sh
@@ -8,6 +8,7 @@ export WorkingDirectory=/home/ghannig/git/Hannigan-2016-ConjunctisViribus/data
 export Output='PfamDomainInteractions'
 
 export PfamDatabase=/mnt/EXT/Schloss-data/reference/Pfam/Pfam-A.hmm
+export InteractionReference=/mnt/EXT/Schloss-data/reference/DomineInteractionDb/PfamAccInteractions.tsv
 
 export MothurProg=/share/scratch/schloss/mothur/mothur
 
@@ -35,7 +36,9 @@ PfamDomains () {
 	mkdir ./${Output}/PfamDomains
 
 	# Translate the sequences using default axiom script
-	perl ${MicroToolkit}TranslateFasta.pl -f ${2} -o ./${Output}/PfamDomains/${1}-TanslatedOrfs.fa
+	perl ${MicroToolkit}TranslateFasta.pl \
+		-f ${2} \
+		-o ./${Output}/PfamDomains/${1}-TanslatedOrfs.fa
 
 	# Perform HMM alignment against pfam HMMER database
 	${hmmerBin}hmmscan \
@@ -55,10 +58,66 @@ PfamDomains () {
 		> ./${Output}/PfamDomains/${1}-PfamDomainsFormat.tsv
 }
 
+OrfInteractionPairs () {
+	# 1 = Phage  Pfam Results
+	# 2 = Bacterial  Pfam Results
+	# 3 = Interaction Reference
+
+	# Reverse the interaction reference for awk
+	awk \
+		'{ print $2"\t"$1 }' \
+		${3} \
+		> ${3}.inverse
+
+	cat \
+		${3} \
+		${3}.inverse \
+		> ./${Output}/TotalInteractionRef.tsv
+
+	# Get only the ORF IDs and corresponding interactions
+	# Column 1 is the ORF ID, two is Uniprot ID
+	cut -f 2,4 ${1} \
+		| sed 's/\.orf\d\+//' \
+		| sed 's/\.\d\+\t/\t/' \
+		> ./${Output}/PfamDomains/PhagePfamAcc.tsv
+
+	cut -f 2,4 ${2} \
+		| sed 's/\.orf\d\+//' \
+		| sed 's/\.\d\+\t/\t/' \
+		> ./${Output}/PfamDomains/BacteriaPfamAcc.tsv
+
+	# Convert bacterial file to reference
+	awk \
+		'NR == FNR {a[$1] = $2; next} { print $1"\t"$2"\t"a[$1] }' \
+		./${Output}/PfamDomains/PhagePfamAcc.tsv \
+		./${Output}/TotalInteractionRef.tsv \
+		> ./${Output}/PfamDomains/tmpMerge.tsv
+
+	awk \
+		'NR == FNR {a[$2] = $1; next} { print $1"\t"$2"\t"$3"\t"a[$3] }' \
+		./${Output}/PfamDomains/BacteriaPfamAcc.tsv \
+		./${Output}/tmpMerge.tsv \
+		| cut -f 1,4 \
+		> ./${Output}/InteractiveIds.tsv
+
+	# This output can be used for input into perl script for adding
+	# to the graph database.
+}
+
 export -f PfamDomains
+export -f OrfInteractionPairs
 
 PfamDomains \
 	"Phage" \
 	${PhageOrfs} \
 	${PfamDatabase}
 
+PfamDomains \
+	"Bacteria" \
+	${PhageOrfs} \
+	${PfamDatabase}
+
+OrfInteractionPairs \
+	./${Output}/PfamDomains/Phage-PfamDomainsFormat.tsv \
+	./${Output}/PfamDomains/Bacteria-PfamDomainsFormat.tsv \
+	${InteractionReference}

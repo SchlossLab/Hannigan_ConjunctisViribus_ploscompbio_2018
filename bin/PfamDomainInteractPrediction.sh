e57c62ca6f91c8e97a7c98cab7f8f5a24a7a8314
Geoffrey Hannigan <ghanni@upenn.edu>
2016-02-10 10:14:01 -0500

    Fixed paths to ORFs

--- bin/PfamDomainInteractPrediction.sh
+++ bin/PfamDomainInteractPrediction.sh
@@ -19,8 +19,8 @@ export LocalBin=/home/ghannig/bin/
 export hmmerBin=/mnt/EXT/Schloss-data/bin/hmmer-3.1b2-linux-intel-x86_64/binaries/
 
 # Get the orfs that were already predicted in 'GerMicrobeOrfs.sh'
-export PhageOrfs=/home/ghannig/git/Hannigan-2016-ConjunctisViribus/data/OrfInteractions/PhageGenomeOrfs.fa
-export BacteriaOrfs=/home/ghannig/git/Hannigan-2016-ConjunctisViribus/data/OrfInteractions/BacteriaGenomeOrfs.fa
+export PhageOrfs=/home/ghannig/git/Hannigan-2016-ConjunctisViribus/data/PhageGenomeOrfs.fa
+export BacteriaOrfs=/home/ghannig/git/Hannigan-2016-ConjunctisViribus/data/BacteriaGenomeOrfs.fa
 
 
 # Make the output directory and move to the working directory

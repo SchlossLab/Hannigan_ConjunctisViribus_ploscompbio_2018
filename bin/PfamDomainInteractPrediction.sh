bc6afc3d17448cfaa7da87ec645684aba579671f
Geoffrey Hannigan <ghanni@upenn.edu>
2016-02-10 09:48:10 -0500

    My life is formatting.

--- bin/PfamDomainInteractPrediction.sh
+++ bin/PfamDomainInteractPrediction.sh
@@ -90,7 +90,7 @@ OrfInteractionPairs () {
 		> ./${Output}/PfamDomains/tmpMerge.tsv
 
 	awk \
-		'NR == FNR {a[$2] = $1; next} { print $1"\t"$2"\t"$3"\t"a[$3] }' \
+		'NR == FNR {a[$1] = $2; next} $3 in a { print $1"\t"$2"\t"$3"\t"a[$3] }' \
 		./${Output}/PfamDomains/BacteriaPfamAcc.tsv \
 		./${Output}/PfamDomains/tmpMerge.tsv \
 		| cut -f 1,4 \

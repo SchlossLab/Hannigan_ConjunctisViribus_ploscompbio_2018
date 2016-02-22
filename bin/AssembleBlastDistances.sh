3ac0ffe9bb1a7170df219abc304b8566d58c179f
Geoffrey Hannigan <ghanni@upenn.edu>
2016-02-22 09:25:23 -0500

    Added script for calculating reference dissimilarity by blast.

---
+++ bin/AssembleBlastDistances.sh
@@ -0,0 +1,46 @@
+# AssembleBlastDistances.sh
+# Geoffrey Hannigan
+# Pat Schloss Lab
+# University of Michigan
+# NOTE: Use this script to get the blast similarities
+# between phages and bacteria.
+
+#######################
+# Set the Environment #
+#######################
+export WorkingDirectory=/home/ghannig/git/Hannigan-2016-ConjunctisViribus/data
+export Output='BlastDistance'
+
+export Vsearch=/mnt/EXT/Schloss-data/bin/vsearch-1.9.10-linux-x86_64/bin/vsearch
+
+export PhageGenomes=/home/ghannig/git/Hannigan-2016-ConjunctisViribus/data/phageSVA.fa
+
+# Set working dir
+cd ${WorkingDirectory}
+mkdir ./${Output}
+
+###########################
+# Get Blast Dissimilarity #
+###########################
+BlastSeqs () {
+	# 1 = Sample Name
+	# 2 = Input Fasta
+
+	makeblastdb \
+		-dbtype nucl \
+		-in ${2} \
+		-out ./${Output}/${1}-database
+
+	blastn \
+    	-query ${2} \
+    	-out ./${Output}/${1}-BlastResults.tsv \
+    	-db ./${Output}/${1}-database \
+    	-outfmt 6 \
+    	-evalue 100
+}
+
+export -f BlastSeqs
+
+BlastSeqs \
+	"PhageGenomes" \
+	${PhageGenomes}

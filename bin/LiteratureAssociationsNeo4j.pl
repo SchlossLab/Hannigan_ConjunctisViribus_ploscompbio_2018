3ea7859ef3068d29bff9224e36927f4c534147b3
Geoffrey Hannigan <ghanni@upenn.edu>
2016-03-03 11:30:18 -0500

    A little messy, but adding in uniprot pred and masking kmer for now.

--- bin/LiteratureAssociationsNeo4j.pl
+++ bin/LiteratureAssociationsNeo4j.pl
@@ -40,6 +40,10 @@ my $PhageTarget;
 my $PercentID;
 my $array1;
 my $array2;
+my $kmer;
+my $relate;
+my $phage;
+my $bacteria;
 
 # Startup the neo4j connection using default location
 # Be sure to set username and password as neo4j
@@ -53,7 +57,8 @@ ref $@ ? $@->rethrow : die $@ if $@;
 GetOptions(
     'h|help' => \$opt_help,
     'i|input=s' => \$input,
-    'c|crispr=s' => \$crispr
+    'c|crispr=s' => \$crispr,
+    'k|kmer=s' => \$kmer
 );
 
 pod2usage(-verbose => 1) && exit if defined $opt_help;
@@ -61,8 +66,9 @@ pod2usage(-verbose => 1) && exit if defined $opt_help;
 # Open files
 open(IN, "<$input") || die "Unable to read in $input: $!";
 open(CRISPR, "<$crispr") || die "Unable to read in $crispr: $!";
+open(KMER, "<$kmer") || die "Unable to read in $kmer: $!";
 
-print STDOUT "\n\n\nProgress: Adding Literature Data.\n";
+print STDERR "\n\n\nProgress: Adding Literature Data.\n";
 
 # Parse the input and save into neo4j
 # Get the literature data
@@ -71,7 +77,7 @@ foreach my $line (<IN>) {
     # Start the script by resetting the flag for each iteraction
     # within the file
     if ($line =~ /^ID\s/) {
-        print STDOUT "Resetting counter...\n";
+        print STDERR "Resetting counter...\n";
         $flag = 0;
         $n1 = 0;
         $n2 = 0;
@@ -81,20 +87,19 @@ foreach my $line (<IN>) {
     } elsif ($flag =~ 0 & $line =~ /^OS\s+(\w.+$)/) {
         # File really should already be without spaces though
         ($formname = $1) =~ s/\s/_/g;
-        print STDOUT "Phage is $formname\n";
+        print STDERR "Phage is $formname\n";
         $n1 = REST::Neo4p::Node->new( {Name => $formname} );
         $n1->set_property( {Organism => 'Phage'} );
         $n1->set_labels('Phage',$formname);
-        print "$n1\n";
         $flag = 1;
         next;
     } elsif ($flag =~ 1 & $line =~ /host=\"(.+)\"/) {
         ($FullName = $1) =~ s/\s/_/g;
-        print STDOUT "Host is $FullName\n";
+        print STDERR "Host is $FullName\n";
         $Genus = (split /_/, $FullName)[0];
         $Species = $Genus."_".(split /_/, $FullName)[1];
-        print STDOUT "Host genus is $Genus\n";
-        print STDOUT "Host species is $Species\n";
+        print STDERR "Host genus is $Genus\n";
+        print STDERR "Host species is $Species\n";
         $n2 = REST::Neo4p::Node->new( {Name => $FullName} );
         $n2->set_property( {Genus => $Genus} );
         $n2->set_property( {Species => $Species} );
@@ -119,7 +124,7 @@ foreach my $line (<IN>) {
     }
 }
 
-print STDOUT "\n\n\nProgress: Adding CRISPRs.\n";
+print STDERR "\n\n\nProgress: Adding CRISPRs.\n";
 
 # Add in the CRISPR match data
 foreach my $line (<CRISPR>) {
@@ -128,26 +133,26 @@ foreach my $line (<CRISPR>) {
     $Spacer = (split /\t/, $line)[0];
     $PhageTarget = (split /\t/, $line)[1];
     $PercentID = (split /\t/, $line)[2];
-    print STDOUT "Spacer host is $Spacer.\n";
-    print STDOUT "Phage target is $PhageTarget.\n";
+    print STDERR "Spacer host is $Spacer.\n";
+    print STDERR "Phage target is $PhageTarget.\n";
     my @n11 = REST::Neo4p->get_nodes_by_label( $PhageTarget );
     my @n12 = REST::Neo4p->get_nodes_by_label( $Spacer );
 
     # Create new phage target node if it does not exist
     unless (@n11) {
         ($formname = $PhageTarget) =~ s/\s/_/g;
-        print STDOUT "New CRISPR phage target is $formname\n";
+        print STDERR "New CRISPR phage target is $formname\n";
         $n1 = REST::Neo4p::Node->new( {Name => $formname} );
         $n1->set_property( {Organism => 'Phage'} );
         $n1->set_labels('Phage',$formname);
     }
     unless (@n12) {
         ($FullName = $Spacer) =~ s/\s/_/g;
-        print STDOUT "New spacer host is $FullName\n";
+        print STDERR "New spacer host is $FullName\n";
         $Genus = (split /_/, $FullName)[0];
         $Species = $Genus."_".(split /_/, $FullName)[1];
-        print STDOUT "CRISPR host genus is $Genus\n";
-        print STDOUT "CRISPR host species is $Species\n";
+        print STDERR "CRISPR host genus is $Genus\n";
+        print STDERR "CRISPR host species is $Species\n";
         $n2 = REST::Neo4p::Node->new( {Name => $FullName} );
         $n2->set_property( {Genus => $Genus} );
         $n2->set_property( {Species => $Species} );
@@ -159,15 +164,111 @@ foreach my $line (<CRISPR>) {
     @n11 = REST::Neo4p->get_nodes_by_label( $PhageTarget );
     @n12 = REST::Neo4p->get_nodes_by_label( $Spacer );
 
-    die "ERROR: There are phage target nodes that do not exist: !$\n" unless (@n11);
-    die "ERROR: There are CRISPR spacer nodes that do not exist: !$\n" unless (@n12);
-
     while( $array1 = pop @n11 ) {
         while( $array2 = pop @n12 ) {
             $array2->relate_to($array1, 'CrisprTarget');
         }
     }
 }
+
+foreach my $line (<KMER>) {
+    chomp $line;
+    $phage = (split /\t/, $line)[0];
+    $bacteria = (split /\t/, $line)[1];
+    print STDERR "Phage host is $phage.\n";
+    print STDERR "Bactera target is $bacteria.\n";
+    my @n11 = REST::Neo4p->get_nodes_by_label( $phage );
+    my @n12 = REST::Neo4p->get_nodes_by_label( $bacteria );
+
+    # Create new phage target node if it does not exist
+    unless (@n11) {
+        ($formname = $phage) =~ s/\s/_/g;
+        print STDERR "New CRISPR phage target is $formname\n";
+        $n1 = REST::Neo4p::Node->new( {Name => $formname} );
+        $n1->set_property( {Organism => 'Phage'} );
+        $n1->set_labels('Phage',$formname);
+    }
+    unless (@n12) {
+        ($FullName = $bacteria) =~ s/\s/_/g;
+        print STDERR "New phage host is $FullName\n";
+        $Genus = (split /_/, $FullName)[0];
+        $Species = $Genus."_".(split /_/, $FullName)[1];
+        print STDERR "CRISPR host genus is $Genus\n";
+        print STDERR "CRISPR host species is $Species\n";
+        $n2 = REST::Neo4p::Node->new( {Name => $FullName} );
+        $n2->set_property( {Genus => $Genus} );
+        $n2->set_property( {Species => $Species} );
+        $n2->set_property( {Organism => 'Bacterial_Host'} );
+        $n2->set_labels('Bacterial_Host',$FullName);
+    }
+
+    # Then get the newly created nodes as arrays
+    @n11 = REST::Neo4p->get_nodes_by_label( $phage );
+    @n12 = REST::Neo4p->get_nodes_by_label( $bacteria );
+
+    while( $array1 = pop @n11 ) {
+        while( $array2 = pop @n12 ) {
+            $array2->relate_to($array1, 'UniprotInfects');
+        }
+    }
+}
+
+#print STDERR "\n\n\nProgress: Adding Tetramer Distances.\n";
+
+# # Comment this out for now while I work on other things
+# foreach my $line (<KMER>) {
+#     chomp $line;
+#     my $flag = 0;
+#     my $origin = (split /\t/, $line)[0];
+#     my $destination = (split /\t/, $line)[2];
+#     my $score = (split /\t/, $line)[1];
+#     print STDERR "Processing $origin\n" if ($origin eq $destination);
+#     my @n11 = REST::Neo4p->get_nodes_by_label( $origin );
+#     my @n12 = REST::Neo4p->get_nodes_by_label( $destination );
+#     # Create new target node if it does not exist
+#     unless (@n11) {
+#         # Replace just in case, but spaces should be gone
+#         ($formname = $origin) =~ s/\s/_/g;
+#         print STDERR "New distance origin is $formname\n";
+#         $n1 = REST::Neo4p::Node->new( {Name => $formname} );
+#         $n1->set_property( {Organism => 'Phage'} );
+#         $n1->set_labels('Phage',$formname);
+#         $flag=1;
+#     }
+#     unless (@n12) {
+#         ($formname = $destination) =~ s/\s/_/g;
+#         print STDERR "New distance destination is $formname\n";
+#         $n2 = REST::Neo4p::Node->new( {Name => $formname} );
+#         $n2->set_property( {Organism => 'Phage'} );
+#         $n2->set_labels('Phage',$formname);
+#         $flag = $flag + 2;
+#     }
+#     # Then get the newly created nodes as arrays
+#     if ($flag == 1) {
+#         print STDERR "Relating new origin to old destination.\n";
+#         while( $array2 = pop @n12) {
+#             $relate = $n1->relate_to($array2, 'Tetramer');
+#             $relate->set_property({ BrayCurtis => $score });
+#         }
+#     } elsif ($flag == 2) {
+#         print STDERR "Relating new destination to old origin.\n";
+#         while( $array1 = pop @n11) {
+#             $relate = $array1->relate_to($n2, 'Tetramer');
+#             $relate->set_property({ BrayCurtis => $score });
+#         }
+#     } elsif ($flag == 3) {
+#         print STDERR "Relating new destination to new origin.\n";
+#         $relate = $n1->relate_to($n2, 'Tetramer');
+#         $relate->set_property({ BrayCurtis => $score });
+#     }
+
+#     while( $array1 = pop @n11 ) {
+#         while( $array2 = pop @n12 ) {
+#             $relate = $array2->relate_to($array1, 'Tetramer');
+#             $relate->set_property({ BrayCurtis => $score });
+#         }
+#     }
+# }
 
 
 # See how long it took

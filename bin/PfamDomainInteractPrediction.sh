3ced323d5f20c371808bde6ecff908980ae99afd
Geoffrey Hannigan <ghanni@upenn.edu>
2016-03-19 14:00:39 -0400

    Think I fixed the Pfam awk

--- bin/PfamDomainInteractPrediction.sh
+++ bin/PfamDomainInteractPrediction.sh
@@ -95,16 +95,16 @@ OrfInteractionPairs () {
 	
 	# Convert bacterial file to reference
 	awk \
-		'NR == FNR {a[$2] = $1; next} { print $1"\t"$2"\t"a[$1] }' \
+		'NR == FNR {a[$2] = $1; next} $1 in a { print $1"\t"$2"\t"a[$1] }' \
 		./${Output}/PhageBlastIdReferencePfams.tsv \
 		./${Output}/TotalInteractionRef.tsv \
 		> ./${Output}/tmpMerge.tsv
 
 	awk \
-		'NR == FNR {a[$2] = $1; next} { print $1"\t"$2"\t"$3"\t"a[$2] }' \
+		'NR == FNR {a[$2] = $1; next} $1 in a { print $1"\t"$2"\t"$3"\t"a[$2] }' \
 		./${Output}/BacteriaBlastIdReferencePfams.tsv \
 		./${Output}/tmpMerge.tsv \
-		# | cut -f 3,4 \
+		| cut -f 3,4 \
 		> ./${Output}/InteractiveIds.tsv
 
 	# This output can be used for input into perl script for adding

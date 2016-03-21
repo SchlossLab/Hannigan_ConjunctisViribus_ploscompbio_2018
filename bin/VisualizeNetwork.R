22d6002f311e7a6237b3106cccd2d98d47752515
Geoffrey Hannigan <ghanni@upenn.edu>
2016-03-21 15:30:58 -0400

    Updated script for visualizing network.

--- bin/VisualizeNetwork.R
+++ bin/VisualizeNetwork.R
@@ -21,12 +21,12 @@ START n=node(*) MATCH (n)-[r]->(m) RETURN n.Name AS from, m.Name AS to, type(r) 
 "
 edges = cypher(graph, query)
 
-## Remove the edges to singleton nodes
-#SingletonNodes <- ddply(edges, c("to"), summarize, length=length(to))
-## Subset because the it is not visible with all small clusters
-#SingletonNodesRemoved <- SingletonNodes[c(SingletonNodes$length > 0),]
-#MultipleEdge <- edges[c(which(edges$to %in% SingletonNodesRemoved$to)),]
-MultipleEdge <- edges
+# Remove the edges to singleton nodes
+SingletonNodes <- ddply(edges, c("to"), summarize, length=length(to))
+# Subset because the it is not visible with all small clusters
+SingletonNodesRemoved <- SingletonNodes[c(SingletonNodes$length > 5),]
+MultipleEdge <- edges[c(which(edges$to %in% SingletonNodesRemoved$to)),]
+# MultipleEdge <- edges
 
 # Set nodes
 nodes = data.frame(id=unique(c(MultipleEdge$from, MultipleEdge$to)))
@@ -38,19 +38,30 @@ ig = graph_from_data_frame(MultipleEdge, directed=F)
 V(ig)$label = ifelse(grepl("[Pp]hage", nodes$id), "", nodes$id)
 V(ig)$label = ""
 V(ig)$color = ifelse(grepl("[Pp]hage", nodes$id), rgb(0,0,1,.75), rgb(1,0,0,.75))
-E(ig)$color <- ifelse(grepl("Infects", edges$type), rgb(0.5,0.5,0.5,.75), rgb(0.5,0,0,.75))
+E(ig)$color <- with(edges, 
+                    ifelse(grepl("Infects_Literature", type), rgb(1,0.25,0.25,0.5),
+                    ifelse(grepl("Infects_CRISPR", type), rgb(0,0.5,1, 0.5),
+                    ifelse(grepl("Infects_Uniprot", type), rgb(0.5,0.5,0,0.5),
+                    ifelse(grepl("Infects_Blast", type), rgb(0.5,0.5,0.5,0.15),
+                    ifelse(grepl("Infects_Pfam", type), rgb(0.5,0,0.5,0.5),
+                    rgb(1,1,1,0.5)
+                    ))))))
 E(ig)$width <- 0.01
 V(ig)$frame.color <- NA
 V(ig)$label.color <- rgb(0,0,.2,.5)
 
 #plot(ig, vertex.size=1.5, edge.arrow.size=.2)
 
-l <- layout.graphopt(ig)
+l <- layout.auto(ig)
 
 pdf(file="/Users/Hannigan/git/Hannigan-2016-ConjunctisViribus/figures/BacteriaPhageNetworkDiagram.pdf", width=8, height=8)
-plot(ig, vertex.size=0.30, edge.arrow.size=.1, layout=l)
+plot(ig, vertex.size=0.30, edge.arrow.size=.1)
+legend('bottomleft', legend=c("Phage", "Bacteria"), pt.bg=c(rgb(0,0,1,.75), rgb(1,0,0,.75)), col='black', pch=21, pt.cex=3, cex=1.5, bty = "n")
+legend('bottomright', legend=c("Infects_Literature", "Infects_CRISPR", "Infects_Uniprot", "Infects_Blast", "Infects_Pfam"), col=c(rgb(1,0.25,0.25,.5), rgb(0,0.5,1, 0.5), rgb(0.5,0.5,0,.5), rgb(0.5,0.5,0.5,.15), rgb(0.5,0,0.5,.5)), pch='-', pt.cex=3, cex=1.5, bty = "n")
 dev.off()
 
 png(file="/Users/Hannigan/git/Hannigan-2016-ConjunctisViribus/figures/BacteriaPhageNetworkDiagram.png", width=8, height=8, units="in", res=800)
 plot(ig, vertex.size=0.30, edge.arrow.size=.1, layout=l)
+legend('bottomleft', legend=c("Phage", "Bacteria"), pt.bg=c(rgb(0,0,1,.75), rgb(1,0,0,.75)), col='black', pch=21, pt.cex=3, cex=1.5, bty = "n")
+legend('bottomright', legend=c("Infects_Literature", "Infects_CRISPR", "Infects_Uniprot", "Infects_Blast"), col=c(rgb(0.5,0,0.5,.5), rgb(0,0.5,1, 0.5), rgb(0.5,0.5,0,.5), rgb(0.5,0.5,0.5,.15)), pch='-', pt.cex=3, cex=1.5, bty = "n")
 dev.off()

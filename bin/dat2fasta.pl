923d860b5974b5b10df58ad7cccbe71de4c00296
Geoffrey Hannigan <ghanni@upenn.edu>
2016-04-04 08:01:38 -0400

    Updated dat parsing script to do either nucleotide or amino acid.

--- bin/dat2fasta.pl
+++ bin/dat2fasta.pl
@@ -22,12 +22,14 @@ my $output;
 my $flag;
 my $formatVar;
 my $sequence;
+my $prot = '';
 
 # Set the options
 GetOptions(
     'h|help' => \$opt_help,
     'd|datInput=s' => \$input,
-    'f|fastaOutput=s' => \$output
+    'f|fastaOutput=s' => \$output,
+    'p|prot' => \$prot
 );
 
 pod2usage(-verbose => 1) && exit if defined $opt_help;
@@ -36,34 +38,67 @@ pod2usage(-verbose => 1) && exit if defined $opt_help;
 open(IN, "<$input") || die "Unable to read in $input: $!";
 open(OUT, ">$output") || die "Unable to write to $output: $!";
 
-foreach my $line (<IN>) {
-    chomp $line;
-    # Start the script by resetting the flag for each iteraction
-    # within the file
-    if ($line =~ /^ID\s/) {
-        print STDERR "Resetting counter...\n";
-        $flag = 0;
-        $formatVar = 0;
-		$sequence = 0;
-        next;
-    } elsif ($flag =~ 0 & $line =~ /^OS\s+(\w.+$)/) {
-        print STDERR "Name is $1\n";
-        print OUT "\>$1\n";
-        $flag = 1;
-        next;
-    } elsif ($flag =~ 1 && $line =~ /^\s+([agct\s]+[agct])\s+[0-9]+$/) {
-        $formatVar = $1;
-        $formatVar =~ s/\s//g;
-        $sequence = $formatVar;
-        $flag = 2;
-    } elsif ($flag =~ 2 && $line =~ /^\s+([agct\s]+[agct])\s+[0-9]+$/) {
-        $formatVar = $1;
-        $formatVar =~ s/\s//g;
-        $sequence = $sequence.$formatVar;
-    } elsif ($flag =~ 2 && $line =~ /^\/\//) {
-        print OUT "$sequence\n";
-    } else {
-        next;
+if ($prot) {
+    foreach my $line (<IN>) {
+        chomp $line;
+        # Start the script by resetting the flag for each iteraction
+        # within the file
+        if ($line =~ /^ID\s/) {
+            print STDERR "Resetting counter...\n";
+            $flag = 0;
+            $formatVar = 0;
+    		$sequence = 0;
+            next;
+        } elsif ($flag =~ 0 & $line =~ /^OS\s+(\w.+$)/) {
+            print STDERR "Name is $1\n";
+            print OUT "\>$1\n";
+            $flag = 1;
+            next;
+        } elsif ($flag =~ 1 && $line =~ /^\s+([A-Z\s]+[A-Z])\s+[0-9]+$/) {
+            $formatVar = $1;
+            $formatVar =~ s/\s//g;
+            $sequence = $formatVar;
+            $flag = 2;
+        } elsif ($flag =~ 2 && $line =~ /^\s+([A-Z\s]+[A-Z])\s+[0-9]+$/) {
+            $formatVar = $1;
+            $formatVar =~ s/\s//g;
+            $sequence = $sequence.$formatVar;
+        } elsif ($flag =~ 2 && $line =~ /^\/\//) {
+            print OUT "$sequence\n";
+        } else {
+            next;
+        }
+    }
+else {
+    foreach my $line (<IN>) {
+        chomp $line;
+        # Start the script by resetting the flag for each iteraction
+        # within the file
+        if ($line =~ /^ID\s/) {
+            print STDERR "Resetting counter...\n";
+            $flag = 0;
+            $formatVar = 0;
+            $sequence = 0;
+            next;
+        } elsif ($flag =~ 0 & $line =~ /^OS\s+(\w.+$)/) {
+            print STDERR "Name is $1\n";
+            print OUT "\>$1\n";
+            $flag = 1;
+            next;
+        } elsif ($flag =~ 1 && $line =~ /^\s+([agct\s]+[agct])\s+[0-9]+$/) {
+            $formatVar = $1;
+            $formatVar =~ s/\s//g;
+            $sequence = $formatVar;
+            $flag = 2;
+        } elsif ($flag =~ 2 && $line =~ /^\s+([agct\s]+[agct])\s+[0-9]+$/) {
+            $formatVar = $1;
+            $formatVar =~ s/\s//g;
+            $sequence = $sequence.$formatVar;
+        } elsif ($flag =~ 2 && $line =~ /^\/\//) {
+            print OUT "$sequence\n";
+        } else {
+            next;
+        }
     }
 }
 

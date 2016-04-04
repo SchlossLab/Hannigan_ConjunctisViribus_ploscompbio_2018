2222586abd4033eaa7ea4b41e5f8c426b29dc79b
Geoffrey Hannigan <ghanni@upenn.edu>
2016-04-04 09:15:02 -0400

    Controlling the output buffer to make life easier for me.

--- bin/dat2fasta.pl
+++ bin/dat2fasta.pl
@@ -25,6 +25,7 @@ my $sequence;
 my $prot = '';
 my $line;
 my $SaveVariable;
+my $bufferCount = 0;
 
 # Set the options
 GetOptions(
@@ -46,6 +47,7 @@ if ($prot) {
         # Start the script by resetting the flag for each iteraction
         # within the file
         if ($line =~ /^ID\s+(\S+)\s/) {
+            $bufferCount = ++$bufferCount;
             $SaveVariable = $1;
             $flag = 0;
             $formatVar = 0;
@@ -75,6 +77,8 @@ if ($prot) {
             next;
         }
     }
+    OUT->flush() if ($bufferCount == 10000);
+    $bufferCount = 0 if ($bufferCount == 10000);
 } else {
     foreach $line (<IN>) {
         chomp $line;

37cbbd64ef479cf22f4261ad27cebef1e1463bcf
Geoffrey Hannigan <ghanni@upenn.edu>
2016-04-19 10:29:12 -0400

    Updated script for dat file parsing to allow protein calls from genome dat or when it is a protein specific dat.

--- bin/dat2fasta.pl
+++ bin/dat2fasta.pl
@@ -16,73 +16,98 @@ use Pod::Usage;
 my $start_run = time();
 
 # Set variables
+my $IN;
+my $OUT;
 my $opt_help;
 my $input;
 my $output;
-my $flag;
+my $flag = 0;
 my $formatVar;
 my $sequence;
 my $prot = '';
 my $line;
 my $SaveVariable;
-my $bufferCount = 0;
-
-hello world
-my variable;
-
+my $genome;
+my $secondVariable;
+my $genomeSaver;
+my $genename;
 
 # Set the options
 GetOptions(
     'h|help' => \$opt_help,
     'd|datInput=s' => \$input,
     'f|fastaOutput=s' => \$output,
-    'p|prot' => \$prot
+    'p|prot' => \$prot,
+    'g|genome' => \$genome
 );
 
 pod2usage(-verbose => 1) && exit if defined $opt_help;
 
 # Open files
-open(IN, "<$input") || die "Unable to read in $input: $!";
-open(OUT, ">$output") || die "Unable to write to $output: $!";
+open($IN, "<", "$input") || die "Unable to read in $input: $!";
+open($OUT, ">", "$output") || die "Unable to write to $output: $!";
 
+# First option if the protein flag is used
 if ($prot) {
-    while ($line=<IN>) {
+    while ($line = <$IN>) {
 	chomp $line;
         # Start the script by resetting the flag for each iteraction
         # within the file
-        if ($line =~ /^ID\s+(\S+)\s/) {
-            $bufferCount = ++$bufferCount;
+        if ($flag == 0 && $line =~ /^ID\s+(\S+)\s/) {
             $SaveVariable = $1;
+            $SaveVariable =~ s/\s/_/g;
             $flag = 0;
             $formatVar = 0;
     		$sequence = 0;
             next;
-        } elsif ($flag =~ 0 & $line =~ /^AC\s+(\w.+)\;$/) {
-            print OUT ">sp\|$1\|$SaveVariable ";
-            $SaveVariable = '';
+        } elsif ($flag == 0 && $line =~ /^AC\s+(\w.+)\;$/) {
+            $secondVariable = ">sp\|$1\|$SaveVariable";
+            # $SaveVariable = '';
             $flag = 0;
             next;
-        } elsif ($flag =~ 0 & $line =~ /^OS\s+(\w.+$)/) {
-            print OUT "$1\n";
+        } elsif ($flag == 0 && $line =~ /^OS\s+(\w.+$)/) {
+            print $OUT ">$secondVariable $1\n" unless ($genome);
+            $genomeSaver = "$secondVariable $1" if ($genome);
             $flag = 1;
+            # undef $secondVariable;
             next;
-        } elsif ($flag =~ 1 && $line =~ /^\s+([A-Z\s]+[A-Z\s])$/) {
+        # If this is not a genome protein file set
+        } elsif ($flag == 1 && $line =~ /^\s+([A-Z\s]+[A-Z\s])$/ && !$genome) {
+            print "error\n";
             $formatVar = $1;
             $formatVar =~ s/\s//g;
             $sequence = $formatVar;
             $flag = 2;
-        } elsif ($flag =~ 2 && $line =~ /^\s+([A-Z\s]+[A-Z\s])$/) {
+        } elsif ($flag == 2 && $line =~ /^\s+([A-Z\s]+[A-Z\s])$/ && !$genome) {
+            print "error\n";
             $formatVar = $1;
             $formatVar =~ s/\s//g;
             $sequence = $sequence.$formatVar;
-        } elsif ($flag =~ 2 && $line =~ /^\/\//) {
-            print OUT "$sequence\n";
-        } else {
-            next;
+        # If these genes are from a genome file
+        } elsif ($flag == 1 && $line =~ /^FT\s+\/product\="(.+)"$/ && $genome) {
+            $genename = $1;
+            $genename =~ s/\s/_/g;
+            print $OUT "$genomeSaver || $genename\n";
+            $flag = 2;
+        } elsif ($flag == 2 && $line =~ /^FT\s+\/translation\=\"([A-Z]+)$/ && $genome) {
+            $formatVar = $1;
+            $formatVar =~ s/\s//g;
+            $sequence = $formatVar;
+            $flag = 3;
+        } elsif ($flag == 3 && $line =~ /^FT\s+([A-Z]+)/ && $genome) {
+            $formatVar = $1;
+            $formatVar =~ s/\s//g;
+            $sequence = $sequence.$formatVar;
+            print $OUT "$sequence\n" if ($line =~ /\"$/);
+            $flag = 1 if ($line =~ /\"$/);
+        } elsif ($flag == 1 && $line =~ /^SQ/ && $genome) {
+            $flag = 2;
+        } elsif ($flag == 2 && $line =~ /^\/\//) {
+            print $OUT "$sequence\n";
         }
     }
 } else {
-    foreach $line (<IN>) {
+    while ($line = <$IN>) {
         chomp $line;
         # Start the script by resetting the flag for each iteraction
         # within the file
@@ -94,7 +119,7 @@ if ($prot) {
             next;
         } elsif ($flag =~ 0 & $line =~ /^OS\s+(\w.+$)/) {
             print STDERR "Name is $1\n";
-            print OUT "\>$1\n";
+            print $OUT "\>$1\n";
             $flag = 1;
             next;
         } elsif ($flag =~ 1 && $line =~ /^\s+([agct\s]+[agct])\s+[0-9]+$/) {
@@ -107,15 +132,15 @@ if ($prot) {
             $formatVar =~ s/\s//g;
             $sequence = $sequence.$formatVar;
         } elsif ($flag =~ 2 && $line =~ /^\/\//) {
-            print OUT "$sequence\n";
+            print $OUT "$sequence\n";
         } else {
             next;
         }
     }
 }
 
-close(IN);
-close(OUT);
+close($IN);
+close($OUT);
 
 # See how long it took
 my $end_run = time();

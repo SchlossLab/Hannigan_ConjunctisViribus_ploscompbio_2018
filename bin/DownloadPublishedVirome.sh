576912a35bc24e37ce582e2b0abbea7897f35e04
Geoffrey Hannigan <ghanni@upenn.edu>
2016-06-14 21:00:58 -0400

    accept glob

--- bin/DownloadPublishedVirome.sh
+++ bin/DownloadPublishedVirome.sh
@@ -37,7 +37,7 @@ DownloadFromSRA () {
 	shortLine=${line:0:6}
 	echo Looking for ${shorterLine} with ${shortLine}
 	# Recursively download the contents of the 
-	wget -r --no-parent -A ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByStudy/sra/${shorterLine}/${shortLine}/${line}/
+	wget -r --no-parent -A "*" ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByStudy/sra/${shorterLine}/${shortLine}/${line}/
 	mv ./ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByStudy/sra/${shorterLine}/${shortLine}/${line}/*/*.sra ./${Output}/"${line}"
 	rm -r ./ftp-trace.ncbi.nih.gov
 }

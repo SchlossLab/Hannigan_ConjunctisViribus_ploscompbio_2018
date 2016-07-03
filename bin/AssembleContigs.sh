6eb4d0e8cc3d5748fdf49cb1fe27ae24a30ee3ee
Geoffrey Hannigan <ghanni@upenn.edu>
2016-07-03 16:02:08 -0400

    Just testing the waters here.

---
+++ bin/AssembleContigs.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+# AssembleContigs.sh
+# Geoffrey Hannigan
+# Pat Schloss Lab
+# University of Michigan
+
+#PBS -N AssembleContigs
+#PBS -q first
+#PBS -l nodes=1:ppn=1,mem=40gb
+#PBS -l walltime=600:00:00
+#PBS -j oe
+#PBS -V
+#PBS -A schloss_lab
+
+#######################
+# Set the Environment #
+#######################
+
+export WorkingDirectory=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/data
+export Output='Assembled Contigs'
+
+export FastqFiles=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/data/PublishedViromeDatasets/qualityTrimmed
+
+export fastxfq2fa=/home/ghannig/bin/fastq_to_fasta
+
+cd ${WorkingDirectory} || exit
+mkdir ./${Output}
+
+###################
+# Set Subroutines #
+###################
+
+ConvertFq2Fa () {
+	# 1 = File Name
+	${fastxfq2fa} -i ${1} -o ${2}
+	rm ${1}
+}
+
+export -f ConvertFq2Fa
+
+################
+# Run Analysis #
+################
+
+ls ${FastqFiles}/*_R1.fastq | xargs -I {} --max-procs=16 sh -c '
+	filename=$(echo {} | sed 's/.*\///')
+	echo Processing file ${filename}...
+'

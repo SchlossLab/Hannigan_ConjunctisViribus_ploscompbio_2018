590da0ceeb83c1a833ba80336948b3afdff2ce52
Geoffrey Hannigan <ghanni@upenn.edu>
2016-07-06 14:39:37 -0400

    Added OPF script.

---
+++ bin/OperationalProteinFamilies.sh
@@ -0,0 +1,105 @@
+#! /bin/bash
+# OperationalProteinFamilies.sh
+# Geoffrey Hannigan
+# Pat Schloss Lab
+# University of Michigan
+
+#PBS -N OperationalProteinFamilies
+#PBS -q first
+#PBS -l nodes=1:ppn=1,mem=40gb
+#PBS -l walltime=600:00:00
+#PBS -j oe
+#PBS -V
+#PBS -A schloss_lab
+
+#######################
+# Set the Environment #
+#######################
+
+export WorkingDirectory=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/data
+export ProjectBin=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/bin
+export Output='OPFs'
+
+export FastaFiles=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/data/AssembledContigs/ContigOrfs
+
+export fastxfq2fa=/home/ghannig/bin/idba-1.1.1/bin/fq2fa
+export idba=/home/ghannig/bin/idba-1.1.1/bin/idba_ud
+export SchlossBin=/scratch/pschloss_flux/ghannig/bin/
+export LocalBin=/home/ghannig/bin/
+
+cd ${WorkingDirectory} || exit
+mkdir ./${Output}
+
+###################
+# Set Subroutines #
+###################
+
+GetProteinHits () {
+	# 1 = Input Orfs
+	# 2 = Reference Orfs
+	# 3 = Output File
+
+	# Create diamond database
+	echo Creating Bacteria Gene Database...
+	${SchlossBin}diamond makedb \
+		--in "${2}" \
+		-d ./${Output}/DiamondReference
+
+	# Use blast to get hits of ORFs to Uniprot genes
+	${SchlossBin}diamond blastp \
+		-q "${1}" \
+		-d ./${Output}/DiamondReference \
+		-a ./${Output}/Blastx.daa \
+		-t ./
+
+	${SchlossBin}diamond view \
+		-a ./${Output}/Blastx.daa \
+		-o "${3}"
+}
+
+EstablishOpfs () {
+	# 1 = Open Reading Frame fasta
+
+	mkdir ./${Output}/OpfBlastDb
+
+	# Blast them to themselves for clustering
+	GetProteinHits \
+		${1} \
+		${1} \
+		./${Output}/OpfBlastResults.blast
+
+    # Cluster the ORFs into OPS using blast output
+    ${MothurProg} "#mgcluster(blast=./${Output}/OpfBlastResults.blast, cutoff=0.75)"
+
+    # Create alignment file
+    ${LocalBin}mafft-linux64/mafft.bat \
+    	${1} \
+    	> ./${Output}/OrfAlignment.fa
+
+    # Create dist matrix for picking representative OPF seqs
+    ${MothurProg} "#dist.seqs(fasta=./${Output}/OrfAlignment.fa, output=lt)"
+
+    # Now get the rep seuqneces
+    ${MothurProg} "#get.oturep(phylip=./${Output}/OrfAlignment.phylip.dist, list=./${Output}/OpfBlastResults.an.list, fasta=${1}, label=0.22)"
+
+    # And format the file
+    sed -i 's/\t/_/g' ./${Output}/OpfBlastResults.an.0.22.rep.fasta
+    sed -i 's/|/_/g' ./${Output}/OpfBlastResults.an.0.22.rep.fasta
+
+	# Make master reference ID list
+	sed -n 1~2p ./${Output}/OpfBlastResults.an.0.22.rep.fasta \
+	| sed s'/>//g' \
+	| sed '1 s/^/Reference_ID\n/' \
+	> ./${Output}/MasterReferenceList.tsv
+}
+
+export -f GetProteinHits
+export -f EstablishOpfs
+
+################
+# Run Analysis #
+################
+
+cat ${FastaFiles}/* > ./${Output}/TotalOrfs.fa
+
+EstablishOpfs ./${Output}/TotalOrfs.fa

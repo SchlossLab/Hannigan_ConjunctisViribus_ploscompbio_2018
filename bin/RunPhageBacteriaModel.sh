ee85075394e47217f70781dd65f8e92fe69c1028
Geoffrey Hannigan <ghanni@upenn.edu>
2016-08-13 11:39:30 -0400

    Added format of rel abund to be added to network database.

--- bin/RunPhageBacteriaModel.sh
+++ bin/RunPhageBacteriaModel.sh
@@ -23,6 +23,7 @@ export GitBin=/mnt/EXT/Schloss-data/ghannig/OpenMetagenomeToolkit/pakbin/
 
 export PhageGenomeRef=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/data/AssembledContigs/FinalContigs/TotalContigs.fa
 export BacteriaGenomeRef=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/data/ValidationSet/ValidationBacteriaNoBlockNoSpace.fa
+export FastaSequences=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ConjunctisViribus/data/AssembledContigs/fastaForAssembly
 
 cd ${WorkingDirectory} || exit
 mkdir ./${Output}
@@ -65,123 +66,145 @@ FormatNames () {
 export -f PredictOrfs
 export -f FormatNames
 
-######################
-# Run CRISPR scripts #
-######################
+# ######################
+# # Run CRISPR scripts #
+# ######################
 
-# Use a tmp directory
-mkdir ./${Output}/tmp
+# # Use a tmp directory
+# mkdir ./${Output}/tmp
 
-echo Extracting CRISPRs...
-bash ${BinPath}RunPilerCr.sh \
-	${BacteriaGenomeRef} \
-	./${Output}/tmp/BenchmarkCrisprs.txt \
-	"/home/ghannig/bin/pilercr1.06/" \
-	|| exit
+# echo Extracting CRISPRs...
+# bash ${BinPath}RunPilerCr.sh \
+# 	${BacteriaGenomeRef} \
+# 	./${Output}/tmp/BenchmarkCrisprs.txt \
+# 	"/home/ghannig/bin/pilercr1.06/" \
+# 	|| exit
 
-echo Getting CRISPR pairs...
-bash ${BinPath}GetCrisprPhagePairs.sh \
-	./${Output}/tmp/BenchmarkCrisprs.txt \
-	${PhageGenomeRef} \
-	./${Output}/BenchmarkCrisprs.tsv \
-	"/home/ghannig/bin/ncbi-blast-2.4.0+/bin/" \
-	${GitBin} \
-	${BinPath} \
-	|| exit
+# echo Getting CRISPR pairs...
+# bash ${BinPath}GetCrisprPhagePairs.sh \
+# 	./${Output}/tmp/BenchmarkCrisprs.txt \
+# 	${PhageGenomeRef} \
+# 	./${Output}/BenchmarkCrisprs.tsv \
+# 	"/home/ghannig/bin/ncbi-blast-2.4.0+/bin/" \
+# 	${GitBin} \
+# 	${BinPath} \
+# 	|| exit
 
-rm ./${Output}/tmp/*
+# rm ./${Output}/tmp/*
 
-# Format the output
-FormatNames \
-	./${Output}/BenchmarkCrisprs.tsv \
-	./${Output}/BenchmarkCrisprsFormat.tsv
+# # Format the output
+# FormatNames \
+# 	./${Output}/BenchmarkCrisprs.tsv \
+# 	./${Output}/BenchmarkCrisprsFormat.tsv
 
-#####################
-# Run BLAST scripts #
-#####################
+# #####################
+# # Run BLAST scripts #
+# #####################
 
-echo Getting prophages by blast...
-bash ${BinPath}GetProphagesByBlast.sh \
-	${PhageGenomeRef} \
-	${BacteriaGenomeRef} \
-	./${Output}/BenchmarkProphagesBlastn.tsv \
-	${WorkingDirectory} \
-	"/home/ghannig/bin/ncbi-blast-2.4.0+/bin/" \
-	|| exit
+# echo Getting prophages by blast...
+# bash ${BinPath}GetProphagesByBlast.sh \
+# 	${PhageGenomeRef} \
+# 	${BacteriaGenomeRef} \
+# 	./${Output}/BenchmarkProphagesBlastn.tsv \
+# 	${WorkingDirectory} \
+# 	"/home/ghannig/bin/ncbi-blast-2.4.0+/bin/" \
+# 	|| exit
 
-# Format the output
-FormatNames \
-	./${Output}/BenchmarkProphagesBlastn.tsv \
-	./${Output}/BenchmarkProphagesBlastnFormat.tsv
+# # Format the output
+# FormatNames \
+# 	./${Output}/BenchmarkProphagesBlastn.tsv \
+# 	./${Output}/BenchmarkProphagesBlastnFormat.tsv
 
-# Flip the output
-awk '{print $2"\t"$1"\t"$3}' ./${Output}/BenchmarkProphagesBlastnFormat.tsv \
-	> ./${Output}/BenchmarkProphagesFormatFlip.tsv
+# # Flip the output
+# awk '{print $2"\t"$1"\t"$3}' ./${Output}/BenchmarkProphagesBlastnFormat.tsv \
+# 	> ./${Output}/BenchmarkProphagesFormatFlip.tsv
 
-################
-# Predict ORFs #
-################
+# ################
+# # Predict ORFs #
+# ################
 
-echo Predicting ORFs...
+# echo Predicting ORFs...
 
-PredictOrfs \
-	${PhageGenomeRef} \
-	./${Output}/PhageReferenceOrfs.fa \
-	|| exit
+# PredictOrfs \
+# 	${PhageGenomeRef} \
+# 	./${Output}/PhageReferenceOrfs.fa \
+# 	|| exit
 
-PredictOrfs \
-	${BacteriaGenomeRef} \
-	./${Output}/BacteriaReferenceOrfs.fa \
-	|| exit
+# PredictOrfs \
+# 	${BacteriaGenomeRef} \
+# 	./${Output}/BacteriaReferenceOrfs.fa \
+# 	|| exit
 
-######################
-# Run BLASTx scripts #
-######################
-echo Getting gene matches by blastx...
+# ######################
+# # Run BLASTx scripts #
+# ######################
+# echo Getting gene matches by blastx...
 
-bash ${BinPath}GetPairsByBlastx.sh \
-	./${Output}/PhageReferenceOrfs.fa \
-	./${Output}/BacteriaReferenceOrfs.fa \
-	./${Output}/MatchesByBlastx.tsv \
-	${WorkingDirectory} \
-	"/mnt/EXT/Schloss-data/bin/" \
-	|| exit
+# bash ${BinPath}GetPairsByBlastx.sh \
+# 	./${Output}/PhageReferenceOrfs.fa \
+# 	./${Output}/BacteriaReferenceOrfs.fa \
+# 	./${Output}/MatchesByBlastx.tsv \
+# 	${WorkingDirectory} \
+# 	"/mnt/EXT/Schloss-data/bin/" \
+# 	|| exit
 
-# Format the output
-FormatNames \
-	./${Output}/MatchesByBlastx.tsv \
-	./${Output}/MatchesByBlastxFormat.tsv
+# # Format the output
+# FormatNames \
+# 	./${Output}/MatchesByBlastx.tsv \
+# 	./${Output}/MatchesByBlastxFormat.tsv
 
-# Format to get the right columns in the right order
-awk '{ print $2"\t"$1"\t"$12 }' \
-	./${Output}/MatchesByBlastxFormat.tsv \
-	> MatchesByBlastxFormatOrder.tsv
+# # Format to get the right columns in the right order
+# awk '{ print $2"\t"$1"\t"$12 }' \
+# 	./${Output}/MatchesByBlastxFormat.tsv \
+# 	> MatchesByBlastxFormatOrder.tsv
 
-####################
-# Run Pfam scripts #
-####################
+# ####################
+# # Run Pfam scripts #
+# ####################
 
-echo Getting PFAM interactions...
+# echo Getting PFAM interactions...
 
-bash ${BinPath}PfamDomainInteractPrediction.sh \
-	./${Output}/PhageReferenceOrfs.fa \
-	./${Output}/BacteriaReferenceOrfs.fa \
-	./${Output}/PfamInteractions.tsv \
-	${WorkingDirectory} \
-	"/mnt/EXT/Schloss-data/bin/" \
-	"/home/ghannig/Pfam/" \
-	|| exit
+# bash ${BinPath}PfamDomainInteractPrediction.sh \
+# 	./${Output}/PhageReferenceOrfs.fa \
+# 	./${Output}/BacteriaReferenceOrfs.fa \
+# 	./${Output}/PfamInteractions.tsv \
+# 	${WorkingDirectory} \
+# 	"/mnt/EXT/Schloss-data/bin/" \
+# 	"/home/ghannig/Pfam/" \
+# 	|| exit
 
-# Format the output
-FormatNames \
-	./${Output}/PfamInteractions.tsv \
-	./${Output}/PfamInteractionsFormat.tsv
+# # Format the output
+# FormatNames \
+# 	./${Output}/PfamInteractions.tsv \
+# 	./${Output}/PfamInteractionsFormat.tsv
 
-# Format the output order and score sum
-awk '{ print $1"\t"$3"\t"($2 + $4) }' \
-	./${Output}/PfamInteractionsFormat.tsv \
-	> ./${Output}/PfamInteractionsFormatScored.tsv 
+# # Format the output order and score sum
+# awk '{ print $1"\t"$3"\t"($2 + $4) }' \
+# 	./${Output}/PfamInteractionsFormat.tsv \
+# 	> ./${Output}/PfamInteractionsFormatScored.tsv 
 
-# Flip output
-awk '{print $2"\t"$1"\t"$3}' ./${Output}/PfamInteractionsFormatScored.tsv  \
-	> ./${Output}/PfamInteractionsFormatScoredFlip.tsv
+# # Flip output
+# awk '{print $2"\t"$1"\t"$3}' ./${Output}/PfamInteractionsFormatScored.tsv  \
+# 	> ./${Output}/PfamInteractionsFormatScoredFlip.tsv
+
+#############################
+# Contig Relative Abundance #
+#############################
+
+echo Getting contig relative abundance table...
+
+rm ./${Output}/ContigRelAbundForNetwork.tsv
+
+for file in $(ls ${FastaSequences}/*_merged.fa-bowtie.tsv | sed "s/.*\///g"); do
+	sampleid=${echo ${file} | sed 's/_merged.fa-bowtie.tsv//'}
+	echo Sample ID is ${sampleid}
+
+	# Remove the header
+	sed -e "1d" ${FastaSequences}/${file}-noheader
+
+	awk -v name=${sampleid} '{ print $0"\t"name }' ${FastaSequences}/${file} >> ./${Output}/ContigRelAbundForNetwork.tsv
+done
+
+
+
+

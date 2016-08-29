f0699afc7a00afde995f9e91a947de4b1d160db2
Geoffrey Hannigan <ghanni@upenn.edu>
2016-08-29 17:09:09 -0400

    Fixed to not replace newline with underscore.

--- bin/GetValidationSequences.sh
+++ bin/GetValidationSequences.sh
@@ -13,14 +13,14 @@ AccString=$(cut -f 2 ${PhageValidationAcc} | tr '\n' ',' | sed 's/,$//')
 
 wget "http://www.ebi.ac.uk/ena/data/view/${AccString}&display=fasta" -O ./ValidationPhage.fa
 # Get rid of the block format
-perl ./bin/remove_block_fasta_format.pl ./ValidationPhage.fa ./ValidationPhageNoBlock.fa
-perl -pe 's/^>ENA\S+\s/>/' ./ValidationPhageNoBlock.fa \
+perl ./bin/remove_block_fasta_format.pl ./ValidationPhage.fa ./ValidationPhageNoBlockint.fa
+perl -pe 's/^>ENA\S+\s/>/' ./ValidationPhageNoBlockint.fa \
 	| perl -pe 's/\, .*//' \
 	| perl -pe 's/ complete.*//' \
-	| perl -pe 's/\s/_/g' \
+	| perl -pe 's/\h/_/g' \
 	> "${PhageOutput}"
-# rm ./ValidationPhage.fa
-# rm ./ValidationPhageNoBlock.fa
+rm ./ValidationPhage.fa
+rm ./ValidationPhageNoBlockint.fa
 
 # Also get a fasta for the bacterial genomes being used
 
@@ -28,11 +28,11 @@ AccBacteria=$(cut -f 3 ${BacteriaValidationAcc} | egrep -v 'Taxon' | egrep -v 'N
 
 wget "http://www.ebi.ac.uk/ena/data/view/${AccBacteria}&display=fasta" -O ./ValidationBacteria.fa
 # Get rid of the block format
-perl ./bin/remove_block_fasta_format.pl ./ValidationBacteria.fa ./ValidationBacteriaNoBlock.fa
-perl -pe 's/^>ENA\S+\s/>/' ./ValidationBacteriaNoBlock.fa \
+perl ./bin/remove_block_fasta_format.pl ./ValidationBacteria.fa ./ValidationBacteriaNoBlockint.fa
+perl -pe 's/^>ENA\S+\s/>/' ./ValidationBacteriaNoBlockint.fa \
 	| perl -pe 's/\, .*//' \
 	| perl -pe 's/ complete.*//' \
-	| perl -pe 's/\s/_/g' \
+	| perl -pe 's/\h/_/g' \
 	> "${BacteriaOutput}"
 rm ./ValidationBacteria.fa
-rm ./ValidationBacteriaNoBlock.fa
+rm ./ValidationBacteriaNoBlockint.fa

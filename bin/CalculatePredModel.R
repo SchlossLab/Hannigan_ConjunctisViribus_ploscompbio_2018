80a99a4c668c7ebc2989da767bec68475ecb792b
Geoffrey Hannigan <ghanni@upenn.edu>
2016-09-02 12:22:24 -0400

    Updated prediction model with stacked bar chart.

--- bin/CalculatePredModel.R
+++ bin/CalculatePredModel.R
@@ -6,7 +6,7 @@
 ##################################
 # Install Dependencies if Needed #
 ##################################
-list.of.packages <- c("RNeo4j", "ggplot2", "C50", "caret")
+list.of.packages <- c("RNeo4j", "ggplot2", "C50", "caret", "wesanderson", "plotROC")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -17,7 +17,10 @@ suppressMessages(c(
 library("RNeo4j"),
 library("ggplot2"),
 library("C50"),
-library("caret")
+library("caret"),
+library("wesanderson"),
+library("plotROC"),
+library("cowplot")
 ))
 
 ###################
@@ -29,38 +32,37 @@ getresults <- function(x, direction=TRUE) {
   x[,3:7] <- as.data.frame(sapply(x[,3:7], as.numeric))
   x <- x[,-c(1:2)]
   rownames(x) <- NULL
-  # # Convert blastn to factor
+  # Convert blastn to factor
   # x$Blast <- factor(ifelse(
   #   x$Blast > 0,
   #   "TRUE",
   #   "FALSE"))
+  x$Interaction <- factor(ifelse(
+    x$Interaction > 0,
+    "Interacts",
+    "NotInteracts"))
   return(x)
 }
 
-c50model <- function(x, trialcount=10, percentsplit=0.5) {
-  x <- x[sample(nrow(x)),]
-  # Note this assumes the first column is the category
-  categories <- data.frame(x[,1])
-  values <- data.frame(x[,-1])
-  trainingcount <- round(nrow(x) * percentsplit)
-  totalcount <- nrow(x)
-  testingcount <- trainingcount + 1
-  traincat <- factor(categories[c(1:trainingcount), ])
-  trainvalues <- values[c(1:trainingcount), ]
-  testcat <- factor(categories[c(testingcount:totalcount), ])
-  testvalues <- values[c(testingcount:totalcount), ]
-
-  # Boost the model
-  model <-  C50::C5.0(trainvalues, traincat, trials=trialcount)
-  pred <- predict(model, testvalues, type="class")
-  accuracy <- sum( pred == testcat ) / length( pred )
-  return(list(summary( model ), accuracy))
+caretmodel <- function(x) {
+  x <- x[ rowSums(x[4:5])!=0, ] 
+  fitControl <- trainControl(method = "repeatedcv",
+                           number = 5,
+                           repeats = 10,
+                           classProbs = TRUE,
+                           summaryFunction = twoClassSummary,
+                           savePredictions = TRUE)
+  model <- train(Interaction~., data=x, trControl=fitControl, method="rf", metric="ROC")
+  return(model)
 }
 
-caretc50model <- function(x) {
-  train_control <- trainControl(method="LOOCV")
-  model <- train(Interaction~., data=x, trControl=train_control, method="nb")
-  return(model)
+createexclusiondataframe <- function (x, y) {
+  originallength <- length(x[,1])
+  scoredlength <- length(x[ rowSums(x[4:5])!=0, 1])
+  excludedlength <- originallength - scoredlength
+  df <- data.frame(c(excludedlength, scoredlength), c("Excluded", "Included"), c(y, y))
+  colnames(df) <- c("Count", "ExclusionStatus", "InteractionStatus")
+  return(df)
 }
 
 ################
@@ -101,15 +103,88 @@ r.PFAM as Pfam;
 positivequerydata <- cypher(graph, querypositive)
 negativequerydata <- cypher(graph, querynegative)
 
-# head(positivequerydata)
-# head(negativequerydata)
-
 positivedf <- getresults(positivequerydata)
 negativedf <- getresults(negativequerydata, FALSE)
 
 dfbind <- rbind(positivedf, negativedf)
 dfbind <- data.frame(dfbind[complete.cases(dfbind),])
 
-# c50model(dfbind)
+outmodel <- caretmodel(dfbind)
+outmodel
 
-caretc50model(dfbind)
+Exclusiondf <- rbind(
+  createexclusiondataframe(positivedf, "Interaction"),
+  createexclusiondataframe(negativedf, "NonInteraction")
+)
+
+Exclusiondf$ExclusionStatus <- factor(Exclusiondf$ExclusionStatus)
+
+excludedgraph <- ggplot(Exclusiondf[order(Exclusiondf$ExclusionStatus, decreasing = TRUE),], aes(x=factor(InteractionStatus), y=Count, fill=factor(ExclusionStatus))) +
+  theme_classic() +
+  theme(
+    axis.line.x = element_line(colour = "black"),
+    axis.line.y = element_line(colour = "black"),
+    legend.position = "top") +
+  geom_bar(stat="identity", position="fill") +
+  scale_fill_manual(values = wes_palette("Royal1")[c(2,1)], name = "") +
+  xlab("Interaction Status") +
+  ylab("Percent of Total Samples")
+
+#############
+# Plot Data #
+#############
+roclobster <- ggplot(outmodel$pred, aes(d = obs, m = NotInteracts)) +
+  geom_roc(n.cuts = 0, color = wes_palette("Royal1")[2]) +
+  theme_classic() +
+  theme(
+    axis.line.x = element_line(colour = "black"),
+    axis.line.y = element_line(colour = "black")
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = 1, yend = 1), linetype=2, colour=wes_palette("Royal1")[1]) +
+  ylab("Sensitivity") +
+  xlab(paste("Inverse Specificity"))
+
+# Plot prediction points
+rocdensity <- ggplot(outmodel$pred, aes(x=Interacts, group=obs, fill=obs)) +
+  theme_classic() +
+  theme(
+    axis.line.x = element_line(colour = "black"),
+    axis.line.y = element_line(colour = "black"),
+    legend.position = "bottom"
+    ) +
+  geom_density(alpha=0.25) +
+  scale_fill_manual(values = wes_palette("Royal1"), name = "Prediction Result") +
+  xlab("Predicted Interaction Probability") +
+  ylab("Occurrence Density")
+
+# Get the variable importance
+vardf <- data.frame(varImp(outmodel$finalModel))
+vardf$categories <- rownames(vardf)
+
+importanceplot <- ggplot(vardf, aes(x=categories, y=Overall)) +
+  theme_classic() +
+  theme(
+    axis.line.x = element_line(colour = "black"),
+    axis.line.y = element_line(colour = "black")
+  ) +
+  geom_bar(stat="identity", fill=wes_palette("Royal1")[1]) +
+  xlab("Categories") +
+  ylab("Importance Score")
+
+barplots <- plot_grid(importanceplot, excludedgraph, labels=c("C", "D"), nrow=1)
+horizontal <- plot_grid(rocdensity, barplots, labels=c("B", ""), nrow=2)
+
+pdf(file="./figures/rocCurves.pdf",
+height=6,
+width=12)
+  a <- dev.cur()
+  png(file="./figures/rocCurves.png",
+  height=6,
+  width=12,
+  units="in",
+  res=800)
+    dev.control("enable")
+    plot_grid(roclobster, horizontal, labels=c("A", ""), nrow=1)
+    dev.copy(which=a)
+  dev.off()
+dev.off()

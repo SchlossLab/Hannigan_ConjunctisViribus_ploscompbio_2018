d633b31406800f9ca0a66f73ad63deda5e52132f
Geoffrey Hannigan <ghanni@upenn.edu>
2016-09-12 14:03:21 -0400

    Trying to fix variables in loop.

--- bin/catcontigs.sh
+++ bin/catcontigs.sh
@@ -11,8 +11,8 @@ export ContigDirectory=$1
 export NewContigDirectory=$2
 
 for contigdir in $(ls "${ContigDirectory}" | grep _megahit); do
-	echo Contig directory is "${contigdir}"
+	echo Contig directory is "${ContigDirectory}"/"${contigdir}"
 	samplename=$(echo "${contigdir}" | sed 's/.*\///g' | sed 's/_megahit//')
 	echo Formatting "${contigdir}" as "${samplename}"
-	cp "${contigdir}"/final.contigs.fa "${NewContigDirectory}"/"${samplename}"
+	cp "${ContigDirectory}"/"${contigdir}"/final.contigs.fa "${NewContigDirectory}"/"${samplename}"
 done

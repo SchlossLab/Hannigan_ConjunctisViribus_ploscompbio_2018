6bb90d7e1a8d65b0931a3e0696ef790b3b12181c
Geoffrey Hannigan <ghanni@upenn.edu>
2016-09-12 13:54:24 -0400

    Trying to update quoting.

--- bin/catcontigs.sh
+++ bin/catcontigs.sh
@@ -10,8 +10,9 @@
 export ContigDirectory=$1
 export NewContigDirectory=$2
 
-for contigdir in $(${ContigDirectory}*_megahit); do
-	samplename=$(echo ${contigdir} | sed 's/.*\///g' | sed 's/_megahit//')
-	echo Formatting ${contigdir} as ${samplename}
-	cp ${contigdir}/final.contigs.fa ${NewContigDirectory}/${samplename}
+for contigdir in $("${ContigDirectory}"*_megahit); do
+	echo Contig directory is "${contigdir}"
+	samplename=$(echo "${contigdir}" | sed 's/.*\///g' | sed 's/_megahit//')
+	echo Formatting "${contigdir}" as "${samplename}"
+	cp "${contigdir}"/final.contigs.fa "${NewContigDirectory}"/"${samplename}"
 done

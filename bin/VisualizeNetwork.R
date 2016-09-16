37f68bfdada4518d5ea5194d83ff65b98644c6a6
Geoffrey Hannigan <ghanni@upenn.edu>
2016-09-16 10:30:47 -0400

    Added library install and load idiom.

--- bin/VisualizeNetwork.R
+++ bin/VisualizeNetwork.R
@@ -6,12 +6,10 @@
 ##################
 # Load Libraries #
 ##################
-library("igraph")
-library("visNetwork")
-library("RNeo4j")
-library("scales")
-library("plyr")
-library("ggplot2")
+packagelist <- c("RNeo4j", "ggplot2", "wesanderson", "igraph", "visNetwork", "scales", "plyr")
+new.packages <- packagelist[!(packagelist %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(packagelist, library, character.only = TRUE)
 
 ###################
 # Set Subroutines #

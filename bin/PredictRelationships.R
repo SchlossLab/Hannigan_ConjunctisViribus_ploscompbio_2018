f19b7b6cc24cc06890ab6df475f88d328e086692
Geoffrey Hannigan <ghanni@upenn.edu>
2016-09-16 10:44:36 -0400

    Okay try this.

--- bin/PredictRelationships.R
+++ bin/PredictRelationships.R
@@ -10,7 +10,7 @@ write("Predicting Phage-Bacteria Interactions", stderr())
 ##################################
 packagelist <- c("RNeo4j", "ggplot2", "optparse", "caret", "wesanderson", "plotROC")
 new.packages <- packagelist[!(packagelist %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages, repos="http://cran.mtu.edu/")
+if(length(new.packages)) install.packages(new.packages, repos='http://cran.us.r-project.org')
 lapply(packagelist, library, character.only = TRUE)
 
 option_list <- list(

f822a6efef2bed11922f3e3fe97c122da2adba03
Geoffrey Hannigan <ghanni@upenn.edu>
2016-10-12 10:40:37 -0400

    Added echos to the table parsing script.

--- bin/SepAbundanceTable.sh
+++ bin/SepAbundanceTable.sh
@@ -16,21 +16,26 @@ mkdir ${tmpdir}
 
 # Get list of sample IDs for each group
 ## Bacteria
+echo Getting bacteria sample list...
 cut -f 3,10 ${metadata} \
 	| grep Bacteria \
 	| awk '{ print $1 }' \
 	> "${tmpdir}"/BacteriaSampleList.tsv
+
 ## VLP
+echo Getting VLP sample list...
 cut -f 3,10 ${metadata} \
 	| grep VLP \
 	| awk '{ print $1 }' \
 	> "${tmpdir}"/VLPSampleList.tsv
 
 # Get list of the contigs associated with each group
+echo Getting list of bacteria contig IDs
 egrep '>' ${bacterialcontigs} \
 	| sed 's/>//' \
 	> "${tmpdir}"/BacteriaContigList.tsv
 
+echo Getting list of phage contig IDs
 egrep '>' ${phagecontigs} \
 	| sed 's/>//' \
 	> "${tmpdir}"/PhageContigList.tsv
@@ -39,10 +44,16 @@ egrep '>' ${phagecontigs} \
 # those samples and contigs from the bacteria or
 # phage sets.
 
+echo Parsing bactieral abundance table...
 grep --file="${tmpdir}"/BacteriaSampleList.tsv \
 	| grep --file="${tmpdir}"/BacteriaContigList.tsv \
 	> ${bacterialabundout}
 
+echo Parsing phage abundance table...
 grep --file="${tmpdir}"/PhageSampleList.tsv \
 	| grep --file="${tmpdir}"/PhageContigList.tsv \
 	> ${phageabundout}
+
+echo Removing tmp directory...
+rm -r ${tmpdir}
+

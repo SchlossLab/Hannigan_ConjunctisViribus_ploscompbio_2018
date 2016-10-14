4395e680a02cdb9a7c8ee2ef2b4be6fd804e2d32
Geoffrey Hannigan <ghanni@upenn.edu>
2016-10-14 10:12:23 -0400

    Final major fix to metadata

--- bin/AddMetadata.sh
+++ bin/AddMetadata.sh
@@ -5,8 +5,12 @@ module load perl/5.22.1
 module load perl-modules/5.22.1
 module load R/3.2.3
 
-export samples=$1
-export metadata=$2
+export samplesPhage=$1
+export sampleBacteria=$2
+export metadata=$3
+
+# Put together the two abundance files
+cat ${samplesPhage} ${sampleBacteria} > ./tmpAbundance.tsv
 
 # # Remove proxy env variables before running the perl script
 # unset http_proxy https_proxy ftp_proxy no_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY NO_PROXY
@@ -17,9 +21,13 @@ export metadata=$2
 echo Running neo4j script...
 
 perl ./bin/Metadata2graph.pl \
-	-s ${samples} \
+	-s ./tmpAbundance.tsv \
 	-m ${metadata} \
 	|| /mnt/EXT/Schloss-data/bin/neo4j-enterprise-2.3.0/bin/neo4j stop
 
 # Stop local neo4j server
 /mnt/EXT/Schloss-data/bin/neo4j-enterprise-2.3.0/bin/neo4j stop
+
+echo Cleaning up files...
+
+./tmpAbundance.tsv

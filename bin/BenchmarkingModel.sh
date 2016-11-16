beec75ce0faa2e40ac2ed4258fba170ce16de7b2
Geoffrey Hannigan <ghanni@upenn.edu>
2016-11-16 10:27:21 -0500

    Only run CRISPRs.

--- bin/BenchmarkingModel.sh
+++ bin/BenchmarkingModel.sh
@@ -93,110 +93,110 @@ FormatNames \
 sed -i 's/_[0-9][0-9]\?[0-9]\?\t/\t/g' ${CRISPRout}
 
 
-#####################
-# Run BLAST scripts #
-#####################
+# #####################
+# # Run BLAST scripts #
+# #####################
 
-echo Getting prophages by blast...
-bash ./bin/GetProphagesByBlast.sh \
-	${PhageGenomeRef} \
-	${BacteriaGenomeRef} \
-	./data/${Output}/BenchmarkProphagesBlastn.tsv \
-	${WorkingDirectory} \
-	"/home/ghannig/bin/ncbi-blast-2.4.0+/bin/" \
-	|| exit
+# echo Getting prophages by blast...
+# bash ./bin/GetProphagesByBlast.sh \
+# 	${PhageGenomeRef} \
+# 	${BacteriaGenomeRef} \
+# 	./data/${Output}/BenchmarkProphagesBlastn.tsv \
+# 	${WorkingDirectory} \
+# 	"/home/ghannig/bin/ncbi-blast-2.4.0+/bin/" \
+# 	|| exit
 
-# Format the output
-FormatNames \
-	./data/${Output}/BenchmarkProphagesBlastn.tsv \
-	./data/${Output}/BenchmarkProphagesBlastnFormat.tsv
+# # Format the output
+# FormatNames \
+# 	./data/${Output}/BenchmarkProphagesBlastn.tsv \
+# 	./data/${Output}/BenchmarkProphagesBlastnFormat.tsv
 
-# Flip the output
-awk '{print $2"\t"$1"\t"$3}' ./data/${Output}/BenchmarkProphagesBlastnFormat.tsv \
-	> ${ProphageOutFile}
+# # Flip the output
+# awk '{print $2"\t"$1"\t"$3}' ./data/${Output}/BenchmarkProphagesBlastnFormat.tsv \
+# 	> ${ProphageOutFile}
 
-################
-# Predict ORFs #
-################
+# ################
+# # Predict ORFs #
+# ################
 
-echo Predicting ORFs...
+# echo Predicting ORFs...
 
-PredictOrfs \
-	${PhageGenomeRef} \
-	./data/${Output}/PhageReferenceOrfs.fa \
-	|| exit
+# PredictOrfs \
+# 	${PhageGenomeRef} \
+# 	./data/${Output}/PhageReferenceOrfs.fa \
+# 	|| exit
 
-PredictOrfs \
-	${BacteriaGenomeRef} \
-	./data/${Output}/BacteriaReferenceOrfs.fa \
-	|| exit
+# PredictOrfs \
+# 	${BacteriaGenomeRef} \
+# 	./data/${Output}/BacteriaReferenceOrfs.fa \
+# 	|| exit
 
-######################
-# Run BLASTx scripts #
-######################
-echo Getting gene matches by blastx...
+# ######################
+# # Run BLASTx scripts #
+# ######################
+# echo Getting gene matches by blastx...
 
-bash ./bin/GetPairsByBlastx.sh \
-	./data/${Output}/PhageReferenceOrfs.fa \
-	./data/${Output}/BacteriaReferenceOrfs.fa \
-	./data/${Output}/MatchesByBlastx.tsv \
-	${WorkingDirectory} \
-	"/mnt/EXT/Schloss-data/bin/" \
-	|| exit
+# bash ./bin/GetPairsByBlastx.sh \
+# 	./data/${Output}/PhageReferenceOrfs.fa \
+# 	./data/${Output}/BacteriaReferenceOrfs.fa \
+# 	./data/${Output}/MatchesByBlastx.tsv \
+# 	${WorkingDirectory} \
+# 	"/mnt/EXT/Schloss-data/bin/" \
+# 	|| exit
 
-# Format the output
-FormatNames \
-	./data/${Output}/MatchesByBlastx.tsv \
-	./data/${Output}/MatchesByBlastxFormat.tsv
+# # Format the output
+# FormatNames \
+# 	./data/${Output}/MatchesByBlastx.tsv \
+# 	./data/${Output}/MatchesByBlastxFormat.tsv
 
-# Format to get the right columns in the right order
-awk '{ print $2"\t"$1"\t"$12 }' \
-	./data/${Output}/MatchesByBlastxFormat.tsv \
-	> ./data/${Output}/tmpMatchesByBlastxFormat.tsv
+# # Format to get the right columns in the right order
+# awk '{ print $2"\t"$1"\t"$12 }' \
+# 	./data/${Output}/MatchesByBlastxFormat.tsv \
+# 	> ./data/${Output}/tmpMatchesByBlastxFormat.tsv
 
-# Remove underscores at the end of the names
-sed -i 's/_[0-9]*\t/\t/g' ./data/${Output}/tmpMatchesByBlastxFormat.tsv
+# # Remove underscores at the end of the names
+# sed -i 's/_[0-9]*\t/\t/g' ./data/${Output}/tmpMatchesByBlastxFormat.tsv
 
-Rscript ./bin/CollapseGeneScores.R \
-	-i ./data/${Output}/tmpMatchesByBlastxFormat.tsv \
-	-o ${BlastxOut}
+# Rscript ./bin/CollapseGeneScores.R \
+# 	-i ./data/${Output}/tmpMatchesByBlastxFormat.tsv \
+# 	-o ${BlastxOut}
 
-rm ./data/${Output}/tmpMatchesByBlastxFormat.tsv
+# rm ./data/${Output}/tmpMatchesByBlastxFormat.tsv
 
-####################
-# Run Pfam scripts #
-####################
+# ####################
+# # Run Pfam scripts #
+# ####################
 
-echo Getting PFAM interactions...
+# echo Getting PFAM interactions...
 
-bash ./bin/PfamDomainInteractPrediction.sh \
-	./data/${Output}/PhageReferenceOrfs.fa \
-	./data/${Output}/BacteriaReferenceOrfs.fa \
-	./data/${Output}/PfamInteractions.tsv \
-	${WorkingDirectory} \
-	"/mnt/EXT/Schloss-data/bin/" \
-	"/home/ghannig/Pfam/" \
-	|| exit
+# bash ./bin/PfamDomainInteractPrediction.sh \
+# 	./data/${Output}/PhageReferenceOrfs.fa \
+# 	./data/${Output}/BacteriaReferenceOrfs.fa \
+# 	./data/${Output}/PfamInteractions.tsv \
+# 	${WorkingDirectory} \
+# 	"/mnt/EXT/Schloss-data/bin/" \
+# 	"/home/ghannig/Pfam/" \
+# 	|| exit
 
-# Format the output
-FormatNames \
-	./data/${Output}/PfamInteractions.tsv \
-	./data/${Output}/PfamInteractionsFormat.tsv
+# # Format the output
+# FormatNames \
+# 	./data/${Output}/PfamInteractions.tsv \
+# 	./data/${Output}/PfamInteractionsFormat.tsv
 
-# Format the output order and score sum
-awk '{ print $1"\t"$3"\t"($2 + $4) }' \
-	./data/${Output}/PfamInteractionsFormat.tsv \
-	> ./data/${Output}/PfamInteractionsFormatScored.tsv 
+# # Format the output order and score sum
+# awk '{ print $1"\t"$3"\t"($2 + $4) }' \
+# 	./data/${Output}/PfamInteractionsFormat.tsv \
+# 	> ./data/${Output}/PfamInteractionsFormatScored.tsv 
 
-# Flip output
-awk '{print $2"\t"$1"\t"$3}' ./data/${Output}/PfamInteractionsFormatScored.tsv  \
-	> ./data/${Output}/tmpPfamInteractionsFormatScored.tsv
+# # Flip output
+# awk '{print $2"\t"$1"\t"$3}' ./data/${Output}/PfamInteractionsFormatScored.tsv  \
+# 	> ./data/${Output}/tmpPfamInteractionsFormatScored.tsv
 
-# Remove underscores at the end of the names
-sed -i 's/_[0-9]*\t/\t/g' ./data/${Output}/tmpPfamInteractionsFormatScored.tsv
+# # Remove underscores at the end of the names
+# sed -i 's/_[0-9]*\t/\t/g' ./data/${Output}/tmpPfamInteractionsFormatScored.tsv
 
-Rscript ./bin/CollapseGeneScores.R \
-	-i ./data/${Output}/tmpPfamInteractionsFormatScored.tsv \
-	-o ${PfamOut}
+# Rscript ./bin/CollapseGeneScores.R \
+# 	-i ./data/${Output}/tmpPfamInteractionsFormatScored.tsv \
+# 	-o ${PfamOut}
 
-rm ./data/${Output}/tmpPfamInteractionsFormatScored.tsv
+# rm ./data/${Output}/tmpPfamInteractionsFormatScored.tsv

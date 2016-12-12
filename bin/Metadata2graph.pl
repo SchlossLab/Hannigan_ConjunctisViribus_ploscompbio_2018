16614a72ef009586a227dcf13dd7217ff9f45500
Geoffrey Hannigan <ghanni@upenn.edu>
2016-12-12 12:53:43 -0500

    Rerunning metadata to debug.

--- bin/Metadata2graph.pl
+++ bin/Metadata2graph.pl
@@ -42,43 +42,43 @@ my $abund = 0;
 my $sampleid = 0;
 my $n1;
 
-foreach my $line (<$SAMPLES>) {
-	chomp $line;
-	$phageid = (split /\t/, $line)[0];
-	print "Organism ID is $phageid\n";
-	$abund = (split /\t/, $line)[2];
-	$sampleid = (split /\t/, $line)[1];
-	print "Sample ID is $sampleid\n";
+# foreach my $line (<$SAMPLES>) {
+# 	chomp $line;
+# 	$phageid = (split /\t/, $line)[0];
+# 	print "Organism ID is $phageid\n";
+# 	$abund = (split /\t/, $line)[2];
+# 	$sampleid = (split /\t/, $line)[1];
+# 	print "Sample ID is $sampleid\n";
 
-	my @n11 = REST::Neo4p->get_nodes_by_label( $sampleid );
-	my @n12 = REST::Neo4p->get_nodes_by_label( $phageid );
-	# print scalar(@n12)."\n";
+# 	my @n11 = REST::Neo4p->get_nodes_by_label( $sampleid );
+# 	my @n12 = REST::Neo4p->get_nodes_by_label( $phageid );
+# 	# print scalar(@n12)."\n";
 
-	# Ensure there are no duplicated nodes
-    die "You have duplicate sample node IDs: $!" if (scalar(@n11) gt 1);
-    die "You have duplicate phage node IDs: $!" if (scalar(@n12) gt 1);
-    next if (scalar(@n12) eq 0);
+# 	# Ensure there are no duplicated nodes
+#     die "You have duplicate sample node IDs: $!" if (scalar(@n11) gt 1);
+#     die "You have duplicate phage node IDs: $!" if (scalar(@n12) gt 1);
+#     next if (scalar(@n12) eq 0);
 
-	unless (@n11) {
-		$n1 = REST::Neo4p::Node->new( {Name => $sampleid} );
-		$n1->set_property( {Organism => 'SampleID'} );
-		$n1->set_labels('SampleID',$sampleid);
-	}
+# 	unless (@n11) {
+# 		$n1 = REST::Neo4p::Node->new( {Name => $sampleid} );
+# 		$n1->set_property( {Organism => 'SampleID'} );
+# 		$n1->set_labels('SampleID',$sampleid);
+# 	}
 
-	@n11 = REST::Neo4p->get_nodes_by_label( $sampleid );
-	@n12 = REST::Neo4p->get_nodes_by_label( $phageid );
-	print scalar(@n11)."\n";
-	print scalar(@n12)."\n";
+# 	@n11 = REST::Neo4p->get_nodes_by_label( $sampleid );
+# 	@n12 = REST::Neo4p->get_nodes_by_label( $phageid );
+# 	print scalar(@n11)."\n";
+# 	print scalar(@n12)."\n";
 
-	my $array1 = pop @n11;
-    my $array2 = pop @n12;
+# 	my $array1 = pop @n11;
+#     my $array2 = pop @n12;
 
-	# Ensure there are no duplicated nodes
-    die "You have duplicate sample node IDs: $!" if (scalar(@n11) gt 1);
-    die "You have duplicate phage node IDs: $!" if (scalar(@n12) gt 1);
+# 	# Ensure there are no duplicated nodes
+#     die "You have duplicate sample node IDs: $!" if (scalar(@n11) gt 1);
+#     die "You have duplicate phage node IDs: $!" if (scalar(@n12) gt 1);
 
-	$array1->relate_to($array2, 'Sampled')->set_property({Abundance => $abund});
-}
+# 	$array1->relate_to($array2, 'Sampled')->set_property({Abundance => $abund});
+# }
 
 my $disease;
 my $studyid;
@@ -108,6 +108,7 @@ foreach my $line (<$META>) {
 	$hosttype = $linearray[11];
 	$subjectid = $linearray[13];
 	$timepoint = $linearray[14];
+	print STDERR "Subject name is $subjectid\n";
 
 	# Skip the header
 	next if ($studyid eq "SRA_Study_s");

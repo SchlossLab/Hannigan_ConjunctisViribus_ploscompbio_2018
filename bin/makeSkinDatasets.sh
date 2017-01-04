5395faf438e585b77c3ede5383d718918e11b7e4
Geoffrey Hannigan <ghanni@upenn.edu>
2017-01-04 07:04:37 -0500

    Updates to efficiently run rneo4j.

--- bin/makeSkinDatasets.sh
+++ bin/makeSkinDatasets.sh
@@ -10,6 +10,7 @@ SKINSITES=(Ax Ac Pa Tw Um Fh Ra Oc)
 # Loop through the array with new neo4j instances
 for i in "${SKINSITES[@]}"
 do
+	echo Processing skin site $i
 	../../bin/neo4j-enterprise-2.3.0/bin/neo4j start
 	Rscript ./bin/GetSkinGraphs.R \
 		--location ${i} \

4275a6c4c84e0f71b3269caf5e64208def026d42
Geoffrey Hannigan <ghanni@upenn.edu>
2017-05-09 08:30:07 -0400

    Just try running blast for a minute.

--- bin/IdentifyContigsBac.sh
+++ bin/IdentifyContigsBac.sh
@@ -17,11 +17,11 @@ RunBlast () {
 	# 1 = Query Seqs
 	# 2 = ReferenceSeqs
 
-	echo Making blast database...
-	${BlastPath}makeblastdb \
-		-dbtype nucl \
-		-in "${2}" \
-		-out ./data/tmpid/ReferenceGenomes
+	# echo Making blast database...
+	# ${BlastPath}makeblastdb \
+	# 	-dbtype nucl \
+	# 	-in "${2}" \
+	# 	-out ./data/tmpid/ReferenceGenomes
 
 	echo Running blastn...
 	${BlastPath}blastn \
@@ -43,49 +43,49 @@ grep -A 1 -f ./data/tmpid/contiglist.tsv ${fastafile} \
 
 RunBlast ./data/tmpid/contigrepset.fa ${referencefile}
 
-# Add cluster ID to the table
-cut -f 1,2 ./data/tmpid/blastout.tsv | sort | uniq > ./data/tmpid/cutblastout.tsv
-grep --file=./data/tmpid/contiglist.tsv ./data/ContigClustersBacteria/clustering_gt2000.csv \
-	| sed 's/\,/\t/' \
-	> ./data/tmpid/clusterids.tsv
-awk -F "\t" 'FNR==NR { a[$1] = $2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$2} }' \
-	./data/tmpid/clusterids.tsv \
-	./data/tmpid/cutblastout.tsv \
-	| sed 's/ENA|\(.\+\)|.*/\1/' \
-	> ./data/tmpid/clustform.tsv
+# # Add cluster ID to the table
+# cut -f 1,2 ./data/tmpid/blastout.tsv | sort | uniq > ./data/tmpid/cutblastout.tsv
+# grep --file=./data/tmpid/contiglist.tsv ./data/ContigClustersBacteria/clustering_gt2000.csv \
+# 	| sed 's/\,/\t/' \
+# 	> ./data/tmpid/clusterids.tsv
+# awk -F "\t" 'FNR==NR { a[$1] = $2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$2} }' \
+# 	./data/tmpid/clusterids.tsv \
+# 	./data/tmpid/cutblastout.tsv \
+# 	| sed 's/ENA|\(.\+\)|.*/\1/' \
+# 	> ./data/tmpid/clustform.tsv
 
-# Make list of IDs
-cut -f 2 ./data/tmpid/clustform.tsv > ./data/tmpid/tmplist.tsv
+# # Make list of IDs
+# cut -f 2 ./data/tmpid/clustform.tsv > ./data/tmpid/tmplist.tsv
 
-grep --file=./data/tmpid/tmplist.tsv ./tmp-database-download/nucl_gb.accession2taxid > ./data/tmpid/taxlist.tsv
+# grep --file=./data/tmpid/tmplist.tsv ./tmp-database-download/nucl_gb.accession2taxid > ./data/tmpid/taxlist.tsv
 
-# Get the taxon IDs
-awk -F "\t" 'FNR==NR { a[$1] = $3; next } { for( i in a ) if($2 ~ i) {print $1"\t"$2"\t"a[$2]} }' \
-	./data/tmpid/taxlist.tsv  \
-	./data/tmpid/clustform.tsv \
-	> ./data/tmpid/ctax.tsv
-cut -f 3 ./data/tmpid/ctax.tsv > ./data/tmpid/tmplist.tsv
-# Download the information using the EBI API
-while read line; do
-	wget -O - "http://www.ebi.ac.uk/ena/data/taxonomy/v1/taxon/tax-id/${line}" \
-	| grep "lineage" \
-	| sed 's/.\+\: \"//' \
-	| sed 's/\; \".*//' \
-	| sed 's/\; /\t/g' \
-	| sed 's/ /_/g' \
-	| sed "s/^/${line}\t/"
-done < ./data/tmpid/tmplist.tsv > ./data/tmpid/taxawnum.tsv
+# # Get the taxon IDs
+# awk -F "\t" 'FNR==NR { a[$1] = $3; next } { for( i in a ) if($2 ~ i) {print $1"\t"$2"\t"a[$2]} }' \
+# 	./data/tmpid/taxlist.tsv  \
+# 	./data/tmpid/clustform.tsv \
+# 	> ./data/tmpid/ctax.tsv
+# cut -f 3 ./data/tmpid/ctax.tsv > ./data/tmpid/tmplist.tsv
+# # Download the information using the EBI API
+# while read line; do
+# 	wget -O - "http://www.ebi.ac.uk/ena/data/taxonomy/v1/taxon/tax-id/${line}" \
+# 	| grep "lineage" \
+# 	| sed 's/.\+\: \"//' \
+# 	| sed 's/\; \".*//' \
+# 	| sed 's/\; /\t/g' \
+# 	| sed 's/ /_/g' \
+# 	| sed "s/^/${line}\t/"
+# done < ./data/tmpid/tmplist.tsv > ./data/tmpid/taxawnum.tsv
 
-rm -rf ./data/tmpcter
-mkdir -p ./data/tmpcter
-for i in $(seq 1 8); do
-	awk -v col=$i '{ if($col) print $col; else print "Unclassified" }' ./data/tmpid/taxawnum.tsv > ./data/tmpcter/${i}.tsv
-done
-paste ./data/tmpcter/* > ./data/tmpid/fullcols.tsv
+# rm -rf ./data/tmpcter
+# mkdir -p ./data/tmpcter
+# for i in $(seq 1 8); do
+# 	awk -v col=$i '{ if($col) print $col; else print "Unclassified" }' ./data/tmpid/taxawnum.tsv > ./data/tmpcter/${i}.tsv
+# done
+# paste ./data/tmpcter/* > ./data/tmpid/fullcols.tsv
 
-awk -F "\t" 'FNR==NR { a[$3] = $1"\t"$2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$0} }' \
-	./data/tmpid/ctax.tsv  \
-	./data/tmpid/fullcols.tsv \
-	> ./data/contigclustersidentity/clustax.tsv
+# awk -F "\t" 'FNR==NR { a[$3] = $1"\t"$2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$0} }' \
+# 	./data/tmpid/ctax.tsv  \
+# 	./data/tmpid/fullcols.tsv \
+# 	> ./data/contigclustersidentity/clustax.tsv
 
 

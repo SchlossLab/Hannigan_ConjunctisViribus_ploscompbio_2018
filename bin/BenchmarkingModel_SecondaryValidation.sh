58afe58d97849d52fc3711ba776ba1edcd67b33e
Geoffrey Hannigan <ghanni@upenn.edu>
2018-02-24 14:38:22 -0500

    I dunno more letters?

--- bin/BenchmarkingModel_SecondaryValidation.sh
+++ bin/BenchmarkingModel_SecondaryValidation.sh
@@ -47,7 +47,7 @@ FormatNames () {
 	# 2 = Output file name
 
 	# Perl here because the regex are easier
-	perl -pe 's/[A-Za-z][A-Za-z]_\w+_\d_([^_])/\1/g' "${1}" \
+	perl -pe 's/\w\w_\w+_\d_([a-zA-Z])/\1/g' "${1}" \
 		| perl -pe 's/__complete\S+\t/\t/g' \
 		| perl -pe 's/_chromosome\S*\t/\t/g' \
 		> "${2}"

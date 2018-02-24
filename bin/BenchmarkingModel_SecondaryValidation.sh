b2aa434edbbec9f9e00ffa81e9909c9c0b44f547
Geoffrey Hannigan <ghanni@upenn.edu>
2018-02-24 14:17:16 -0500

    Fixed regex for new instance I think.

--- bin/BenchmarkingModel_SecondaryValidation.sh
+++ bin/BenchmarkingModel_SecondaryValidation.sh
@@ -47,7 +47,8 @@ FormatNames () {
 	# 2 = Output file name
 
 	# Perl here because the regex are easier
-	perl -pe 's/\w\w_\w+_\d_([^_])/\1/g' "${1}" \
+	perl -pe 's/^\w\w_\w+_\d_([^_])/\1/' "${1}" \
+		| perl -pe 's/\t\w\w_\w+_\d_([^_])/\t\1/' \
 		| perl -pe 's/__complete\S+\t/\t/g' \
 		| perl -pe 's/_chromosome\S*\t/\t/g' \
 		> "${2}"
